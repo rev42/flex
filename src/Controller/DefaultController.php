95e39b274cb45f98ae73bb43a46e31dd3c5e8455
Olivier Cornu <olivier.cornu@gmail.com>
2017-11-23 14:34:08 -0500

    Added twig (composer req template)

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function index()
+    {
+        return $this->render('default/index.html.twig', ['greeting' => 'hello']);
+    }
+}
