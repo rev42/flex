421fc88497c772bbb5a8faf552fb7543099e72af
Olivier Cornu <olivier.cornu@gmail.com>
2017-12-13 16:19:03 -0500

    Package operations: 2 installs, 18 updates, 2 removals   - Removing doctrine/cache (v1.6.2)   - Removing symfony/stopwatch (v3.3.13)   - Updating symfony/flex (v1.0.41 => v1.0.49): Downloading (100%)   - Updating symfony/debug (v3.3.13 => v3.4.1): Downloading (100%)   - Updating symfony/console (v3.3.13 => v3.4.1): Downloading (100%)   - Updating symfony/routing (v3.3.13 => v3.4.1): Downloading (100%)   - Installing paragonie/random_compat (v2.0.11): Loading from cache   - Installing symfony/polyfill-php70 (v1.6.0): Loading from cache   - Updating symfony/http-foundation (v3.3.13 => v3.4.1): Downloading (100%)   - Updating symfony/event-dispatcher (v3.3.13 => v3.4.1): Downloading (100%)   - Updating symfony/http-kernel (v3.3.13 => v3.4.1): Downloading (100%)   - Updating symfony/finder (v3.3.13 => v3.4.1): Downloading (100%)   - Updating symfony/filesystem (v3.3.13 => v3.4.1): Downloading (100%)   - Updating symfony/dependency-injection (v3.3.13 => v3.4.1): Downloading (100%)   - Updating symfony/config (v3.3.13 => v3.4.1): Downloading (100%)   - Updating symfony/class-loader (v3.3.13 => v3.4.1): Downloading (100%)   - Updating symfony/cache (v3.3.13 => v3.4.1): Downloading (100%)   - Updating symfony/framework-bundle (v3.3.13 => v3.4.1): Downloading (100%)   - Updating symfony/twig-bridge (v3.3.13 => v3.4.1): Downloading (100%)   - Updating symfony/twig-bundle (v3.3.13 => v3.4.1): Downloading (100%)   - Updating symfony/yaml (v3.3.13 => v3.4.1): Downloading (100%)   - Updating symfony/dotenv (v3.3.13 => v3.4.1): Downloading (100%)

---
+++ README.md
@@ -0,0 +1,2 @@
+- Go to public folder
+- Run `php -S localhost:8090`
--- src/Controller/FoController.php
+++ src/Controller/FoController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Service\Checker;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -17,15 +18,21 @@ class FoController extends Controller
      * @var Checker
      */
     private $checker;
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
 
-    public function __construct(Checker $checker, RequestStack $requestStack)
+    public function __construct(LoggerInterface $logger, Checker $checker, RequestStack $requestStack)
     {
         $this->checker = $checker;
         $this->request = $requestStack->getCurrentRequest();
+        $this->logger = $logger;
     }
 
     public function index()
     {
+        $this->logger->warning(__METHOD__);
         return $this->render(
             'default/index.html.twig',
             [
